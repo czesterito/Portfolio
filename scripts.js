70bd0eb3d0d7675c595980bdb0dfdb5e7f8fc08f
czesterito <51289885+czesterito@users.noreply.github.com>
2021-03-18 14:11:50 +0100

    4/5
    bonk

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let open_space = document.getElementById("space-popup");
+let space_btn = document.getElementById("space-btn");
+let space_close_btn = document.getElementById("space-close-btn");
+let open_piano = document.getElementById("piano-popup");
+let piano_btn = document.getElementById("piano-btn");
+let piano_close_btn = document.getElementById("piano-close-btn");
+
+space_btn.onclick = function (){
+    open_space.style.display = "block";
+}
+
+space_close_btn.onclick = function (){
+    open_space.style.display = "none";
+}
+
+piano_btn.onclick = function (){
+    open_piano.style.display = "block";
+}
+
+piano_close_btn.onclick = function (){
+    open_piano.style.display = "none";
+}
