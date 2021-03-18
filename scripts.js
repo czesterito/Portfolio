1355c1ce6a299fbe4b98e172f191e5b814a737c1
czesterito <51289885+czesterito@users.noreply.github.com>
2021-03-18 23:19:39 +0100

    Final stage
    bonk

--- scripts.js
+++ scripts.js
@@ -20,3 +20,17 @@ piano_btn.onclick = function (){
 piano_close_btn.onclick = function (){
     open_piano.style.display = "none";
 }
+
+let burger = document.getElementsByClassName("hamburger")[0];
+let phone_menu = document.getElementsByClassName("phone-menu")[0];
+let cancel = document.getElementById("cancel");
+
+burger.addEventListener("click", function (){
+    phone_menu.style.display = "block";
+    burger.style.display = "none";
+})
+
+cancel.onclick = function (){
+    phone_menu.style.display = "none";
+    burger.style.display = "flex";
+}
